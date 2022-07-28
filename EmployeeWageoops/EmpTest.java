d9c0d9b2f9a9542dee72d160482e89b258990dfb
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 23:27:32 +0530

    Calculate wage for multiple companies

---
+++ EmployeeWageoops/EmpTest.java
@@ -0,0 +1,15 @@
+package EmployeeWageoops;
+
+public class EmpTest {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Employee emp1=new Employee(20, 25, 100, "wipro");
+			emp1.CalculateWage();
+			Employee emp2=new Employee(21, 29, 125, "Tcs");
+			emp2.CalculateWage();
+
+		}
+
+}
