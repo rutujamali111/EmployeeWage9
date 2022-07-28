5877fc79911a21a0998f32eaa6c8c249b8fe1d98
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 08:42:48 +0530

    employee daily wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,13 +3,17 @@ public class EmployeeWage
 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome");
 		int IS_FULL_TIME=1;
-		double empCheck=(Math.random()*10)%2;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck=Math.floor(Math.random()*10)%2;
 		if(empCheck==IS_FULL_TIME)
-			System.out.println("Employee is present..");
+			empHrs=8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs=0;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Daily employee wage: "+ empWage);
 		
 
 	}
