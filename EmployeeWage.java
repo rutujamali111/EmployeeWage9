720f97399385558619c840bcfb12705eedd992e3
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 08:46:30 +0530

    employee daily wage and part time wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,16 +3,19 @@ public class EmployeeWage
 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int IS_FULL_TIME=1;
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
 		int EMP_RATE_PER_HOUR=20;
-		int empHrs=0;
+		int EMPhRS=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==IS_FULL_TIME)
-			empHrs=8;
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if (empCheck==IS_PART_TIME)
+			EMPhRS=4;
+		else if(empCheck==IS_FULL_TIME)
+			EMPhRS=0;
 		else
-			empHrs=0;
-		empWage=empHrs*EMP_RATE_PER_HOUR;
+			EMPhRS=0;
+		empWage=EMPhRS*EMP_RATE_PER_HOUR;
 		System.out.println("Daily employee wage: "+ empWage);
 		
 
