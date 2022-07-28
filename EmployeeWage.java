f2ccb04231f3d6608fc712d738e4f7a7919c54b9
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 08:36:41 +0530

    Check presenty

---
+++ EmployeeWage.java
@@ -0,0 +1,16 @@
+
+public class EmployeeWage 
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		int IS_FULL_TIME=1;
+		double empCheck=(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME)
+			System.out.println("Employee is present..");
+		else
+			System.out.println("Employee is Absent");
+		
+
+	}
+}
