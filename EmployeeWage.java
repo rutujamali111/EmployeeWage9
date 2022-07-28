3eedc0e526fcea18d1e735ec347483d622254794
rutuja_mali <rutujamali111@gmail.com>
2022-07-28 08:53:11 +0530

    employee monthly wage using switch

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,10 +4,14 @@ public class EmployeeWage
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public static final int EMP_RATE_PER_HR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
 	public static void main(String[] args) {
 		int empHrs=0;
 		int empWage=0;
+		int totalEmpWage=0;
 		int empCheck=(int) Math.floor(Math.random()*10)%3;
+		for(int day=0;day<NUM_OF_WORKING_DAYS;day++)
+		{
 		switch(empCheck)
 		{
 		case IS_PART_TIME:
@@ -20,8 +24,10 @@ public class EmployeeWage
 			empHrs=0;
 		}
 		empWage=empHrs*EMP_RATE_PER_HR;
+		totalEmpWage+=empWage;
 
-	System.out.println("Daily employee wage: "+ empWage);	
-
+		System.out.println("Daily employee wage: "+ empWage);
+		
+		}
 	}
 }
